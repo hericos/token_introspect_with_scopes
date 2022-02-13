7d36601374804ff42a13adb054a4d0115e6a03ef
Herico Araujo <hericos@gmail.com>
2022-02-13 14:00:25 -0300

    Add files via upload

---
+++ env.sh
@@ -0,0 +1,32 @@
+export TOKEN_EP=https://localhost:9443/oauth2/token
+
+export AUTHZ_EP=https://localhost:9443/oauth2/authorize
+
+export INTROSPECT_EP=https://localhost:9443/oauth2/introspect
+
+export REVOKE_EP=https://localhost:9443/oauth2/revoke
+
+export PDP_SERVER_URL=https://localhost:9443/api/identity/entitlement/decision/pdp
+
+export SCIM_GROUP_EP=https://localhost:9443/wso2/scim/Groups
+
+export SCIM_USER_EP=https://localhost:9443/wso2/scim/Users
+ 
+
+# client id
+export CLIENTID=Ezljic_AL3f9jtq7elquKwMPVZka
+
+# client secret
+export CLIENTSECRET=BZv0PAc8AA7efRe8RMlBOP8u0f0a
+
+# the username used for both resource owner password grant type and to authenticate to the XACML PDP
+export USERNAME=admin
+
+# the password used for both resource owner password grant type and to authenticate to the XACML PDP
+export PASSWORD=admin
+
+# the scope used in requesting an access token
+export SCOPE=read
+
+# the oauth 2.0 redirect_uri used in authorization code and implicit grant types.
+export REDIRECTURI=http://127.0.0.1:5000/
